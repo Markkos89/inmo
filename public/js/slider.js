6f4582aebd0b30a467a6d32c3ef0321338db2687
romannes <jeremiasgomez990@gmail.com>
2019-05-18 12:48:37 -0300

    nuevas cards

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,66 +1,51 @@
-const slides = document.querySelectorAll('.slide');
-const next = document.querySelector('#next');
-const prev = document.querySelector('#prev');
-const auto = false; // Auto scroll
-const intervalTime = 5000;
-let slideInterval;
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('#next')
+const prev = document.querySelector('#prev')
+const auto = true; // Auto scroll
+const intervalTime = 3500
+let slideInterval
 
 const nextSlide = () => {
-  // Get current class
-  const current = document.querySelector('.current');
-  // Remove current class
-  current.classList.remove('current');
-  // Check for next slide
+  const current = document.querySelector('.current')
+  current.classList.remove('current')
   if (current.nextElementSibling) {
-    // Add current to next sibling
-    current.nextElementSibling.classList.add('current');
+    current.nextElementSibling.classList.add('current')
   } else {
-    // Add current to start
-    slides[0].classList.add('current');
+    slides[0].classList.add('current')
   }
-  setTimeout(() => current.classList.remove('current'));
+  setTimeout(() => current.classList.remove('current'))
 };
 
 const prevSlide = () => {
-  // Get current class
-  const current = document.querySelector('.current');
-  // Remove current class
-  current.classList.remove('current');
-  // Check for prev slide
+  const current = document.querySelector('.current')
+  current.classList.remove('current')
   if (current.previousElementSibling) {
-    // Add current to prev sibling
-    current.previousElementSibling.classList.add('current');
+    current.previousElementSibling.classList.add('current')
   } else {
-    // Add current to last
-    slides[slides.length - 1].classList.add('current');
+    slides[slides.length - 1].classList.add('current')
   }
-  setTimeout(() => current.classList.remove('current'));
-};
-
-// Button events
-next.addEventListener('click', e => {
-  nextSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
-});
-
-prev.addEventListener('click', e => {
-  prevSlide();
-  if (auto) {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(nextSlide, intervalTime);
-  }
-});
-
-// Auto slide
-if (auto) {
-  // Run next slide at interval time
-  slideInterval = setInterval(nextSlide, intervalTime);
+  setTimeout(() => current.classList.remove('current'))
 }
 
- $('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-  });
+next.addEventListener('click', e => {
+  nextSlide()
+  if (auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSlide, intervalTime)
+  }
+})
+
+prev.addEventListener('click', e => {
+  prevSlide()
+  if (auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSlide, intervalTime)
+  }
+})
+
+if (auto) slideInterval = setInterval(nextSlide, intervalTime)
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+})
--- routes.js
+++ routes.js
@@ -2,7 +2,55 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-	res.render('index')
+	const alquileres = [
+		{ 
+			titulo: 'Venta Terreno Comercial en Loteo Colonial', 
+			precio: '$69.990',
+			fotos: [
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+			],
+			descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat explicabo, maxime, assumenda.'
+		},
+		{ 
+			titulo: 'Alquiler Terreno', 
+			precio: '$69.990',
+			fotos: [
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+			],
+			descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat explicabo, maxime, assumenda.'
+		},
+		{ 
+			titulo: 'Otro alquiler', 
+			precio: '$69.990',
+			fotos: [
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+			],
+			descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat explicabo, maxime, assumenda.'
+		},
+		{ 
+			titulo: 'Otra Venta', 
+			precio: '$69.990',
+			fotos: [
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+				{ path: 'background.jpg'},
+				{ path: 'background2.jpg'},
+			],
+			descripcion: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat explicabo, maxime, assumenda.'
+		}
+	]
+	res.render('index', {
+		alquileres
+	})
 })
 
 module.exports = router
