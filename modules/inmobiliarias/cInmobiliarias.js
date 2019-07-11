2bfb19241d4b8b894829b9eb683fe524d46b8d8f
Jeremias Gomez <jeremiasgomez990@gmail.com>
2019-07-10 23:41:28 -0300

    login superadmin y inmobiliaria

--- modules/inmobiliarias/cInmobiliarias.js
+++ modules/inmobiliarias/cInmobiliarias.js
@@ -5,10 +5,19 @@ exports.getAll = (req, res) => {
 }
 
 exports.postLogin = (req, res) => {
-	console.log(req.body)
 	const { usuario, contraseña } = req.body
 	mInmobiliarias.getUserInmobiliaria(usuario, contraseña)
 	.then(data => {
+		console.log(data)
+		req.session.user = data[0]
+        req.session.auth = true
+        req.session.user.rol = "inmobiliaria"
+        req.session.save()
 		res.send(data)
 	}) 
+}
+
+exports.getInicio = (req, res) => {
+	console.log(req.session.user)
+	res.render('inmobiliarias/views/inicio')
 }
--- modules/superuser/cSuperuser.js
+++ modules/superuser/cSuperuser.js
@@ -29,11 +29,25 @@ exports.getInicio = (req, res) => {
 
 exports.postInmobiliarias = async (req, res) => {
     console.log(req.body)
-    await mSuperuser.insertInmobiliaria(req.body)
+    if ( req.body.accion == "N") {
+        await mSuperuser.insertInmobiliaria(req.body)        
+    } else {
+        await mSuperuser.updateInmobiliaria(req.body)
+    }
     res.send({ exito: "OK"})
 }
 
 exports.listaajax = async (req, res) => {
     const inmobiliarias = await mSuperuser.getInmobiliarias()
     res.render('superuser/views/tabla', {inmobiliarias})
+}
+
+exports.getModificarInmobiliaria = async (req, res) => {
+    const inmobiliaria = await mSuperuser.getInmobiliariaById(req.params.id)
+    res.send(inmobiliaria)
+}
+
+exports.getEliminar = async (req, res) => {
+    await mSuperuser.deleteInmobiliaria(req.params.id)
+    res.send({ exito: "OK" })
 }
